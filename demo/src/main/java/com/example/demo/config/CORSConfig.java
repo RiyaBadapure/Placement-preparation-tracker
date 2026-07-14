0f7fb79bd939e9cc66a3e32d39815e7d3a2ceede
RiyaBadapure <riyab2024@gmail.com>
2026-07-14 22:01:57 +0530

    corsss

--- demo/src/main/java/com/example/demo/config/CORSConfig.java
+++ demo/src/main/java/com/example/demo/config/CORSConfig.java
@@ -2,17 +2,18 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CORSConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                     .allowedOrigins("https://placementpreparations.netlify.app")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
